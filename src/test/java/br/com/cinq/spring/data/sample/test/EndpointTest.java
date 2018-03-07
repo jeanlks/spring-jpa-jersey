c05c65428f322458ceaabea613fed6df0266792c
jeanlks <jmontecarvalho@gmail.com>
2018-03-07 14:19:17 -0300

    changed spring boot version

--- src/test/java/br/com/cinq/spring/data/sample/test/EndpointTest.java
+++ src/test/java/br/com/cinq/spring/data/sample/test/EndpointTest.java
@@ -1,43 +1,28 @@
 package br.com.cinq.spring.data.sample.test;
 
-import org.junit.Assert;
+import br.com.cinq.spring.data.sample.application.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import br.com.cinq.spring.data.sample.application.Application;
-import br.com.cinq.spring.data.sample.entity.City;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebIntegrationTest(randomPort = true)
-@IntegrationTest("server.port=9000")
+@SpringBootTest(value = "server.port=9000", classes = Application.class)
 @ActiveProfiles("unit")
 public class EndpointTest {
     Logger logger = LoggerFactory.getLogger(EndpointTest.class);
 
     private final String localhost = "http://localhost:";
 
-    @Value("${local.server.port}")
-    private int port;
+   // @Value("${local.server.port}")
+    //private int port;
 
-    private RestTemplate restTemplate = new TestRestTemplate();
+    private RestTemplate restTemplate = new RestTemplate();
 
     @Test
     public void testGetCities() throws InterruptedException {
